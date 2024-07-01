d09677bf0400799f2180a983cbabe053e380cf3e
Vishnu Rajan Tejus <vrtejus@gmail.com>
2024-07-01 13:03:06 -0700

    Parse mgf file data locally when a peptide is selected

--- frontend/src/initializeGlobals.ts
+++ frontend/src/initializeGlobals.ts
@@ -1,12 +1,21 @@
 import DataUploadStore from "./stores/DataUploadStore";
+import FragmentationStore from "./stores/FragmentationStore";
+import PeptideStore from "./stores/PeptideStore";
+import UserStore from "./stores/UserStore";
 
 declare global {
   interface Window {
     _ipsa: {
       dataUploadStore: typeof DataUploadStore;
+      userStore: typeof UserStore;
+      peptideStore: typeof PeptideStore;
+      fragmentationStore: typeof FragmentationStore;
     };
   }
 }
 window._ipsa = {
   dataUploadStore: DataUploadStore,
+  userStore: UserStore,
+  peptideStore: PeptideStore,
+  fragmentationStore: FragmentationStore,
 };
